1796e33a3625679af2c39b74b65f25ffd50f07a6
Justin Caguiat <justin.caguiat@gmail.com>
2015-02-23 08:44:27 -0500

    fixing slot logic

--- SlotMachineAssignment/Scripts/game.ts
+++ SlotMachineAssignment/Scripts/game.ts
@@ -8,16 +8,19 @@ var buttonBitmap: createjs.Bitmap;
 var game;
 var background;
 var spinButton;
-var slotOne = ["Sonic", "Tails", "YellowGuy", "Robotnic", "Bars", "Knuckles", "Rings", "blanks"];
-var slotTwo = ["Sonic", "Tails", "YellowGuy", "Robotnic", "Bars", "Knuckles", "Rings", "blanks"];
-var slotThree = ["Sonic", "Tails", "YellowGuy", "Robotnic", "Bars", "Knuckles", "Rings", "blanks"];
+var reels = ["Sonic", "Tails", "YellowGuy", "Robotnic", "Bars", "Knuckles", "Rings", "blanks"];
 var firstOutcome;
 var secondOutcome;
 var thirdOutcome;
 
 var spins = 0;
 var win = 0;
+var loss = 0;
 var jackpot = 0;
+var playerBet = 0;
+var winnings = 0;
+
+
 
 // FUNCTIONS ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -53,32 +56,26 @@ function createUI() {
 
 function SpinButton() {
     //Getting Random Elements from each slot
-    firstOutcome = Math.floor(Math.random() * slotOne.length);
-    console.log("Slot One: " + slotOne[firstOutcome]);
+    firstOutcome = Math.floor(Math.random() * reels.length);
+    console.log("Slot One: " + reels[firstOutcome]);
 
-    secondOutcome = Math.floor(Math.random() * slotTwo.length);
-    console.log("Slot Two: " + slotTwo[secondOutcome]);
+    secondOutcome = Math.floor(Math.random() * reels.length);
+    console.log("Slot Two: " + reels[secondOutcome]);
 
-    thirdOutcome = Math.floor(Math.random() * slotThree.length);
-    console.log("Slot Three: " + slotThree[thirdOutcome]);
+    thirdOutcome = Math.floor(Math.random() * reels.length);
+    console.log("Slot Three: " + reels[thirdOutcome]);
 
 
 
-    payoutCheck(slotOne[firstOutcome], slotTwo[secondOutcome], slotThree[thirdOutcome]);
+    payoutCheck(reels[firstOutcome], reels[secondOutcome], reels[thirdOutcome]);
 
 
 }
 
 function payoutCheck(spotOne, spotTwo, SpotThree) {
-    console.log("");
-    spins++;
-    console.log("Number is spins " + spins);
-    console.log("Number is wins " + win);
-    console.log("Number of Jackpots " + jackpot);
-    console.log("jackPot Percentage " + Math.floor(jackpot / spins * 100) + " %");
-    console.log("Win percentage : " + Math.floor(win / spins * 100) + " %");
+
     var allSlots = [spotOne, spotTwo, SpotThree];
-    var possibilities = ["Sonic", "Tails", "YellowGuy", "Robotnic", "Bars", "Knuckles", "Rings", "blanks"];
+
     var sonic = 0;
     var tails = 0;
     var yellowGuy = 0;
@@ -88,148 +85,136 @@ function payoutCheck(spotOne, spotTwo, SpotThree) {
     var rings = 0;
     var blanks = 0;
 
-    var total = [];
-    var highest = 0;
-    var choice = 0;
+    playerBet = 1;
 
-    for (var i = 0; i < possibilities.length; i++) {
+    for (var i = 0; i < reels.length; i++) {
         for (var r = 0; r < allSlots.length; r++) {
-            switch (possibilities[i]) {
-                case possibilities[0]:
-                    if (possibilities[0] == allSlots[r]) {
+            switch (reels[i]) {
+                case reels[0]:
+                    if (reels[0] == allSlots[r]) {
                         sonic++;
                     }
                     break;
-                case possibilities[1]:
-                    if (possibilities[1] == allSlots[r]) {
+                case reels[1]:
+                    if (reels[1] == allSlots[r]) {
                         tails++;
                     }
                     break;
-                case possibilities[2]:
-                    if (possibilities[2] == allSlots[r]) {
+                case reels[2]:
+                    if (reels[2] == allSlots[r]) {
                         yellowGuy++;
                     }
                     break;
-                case possibilities[3]:
-                    if (possibilities[3] == allSlots[r]) {
+                case reels[3]:
+                    if (reels[3] == allSlots[r]) {
                         robotnic++;
                     }
                     break;
-                case possibilities[4]:
-                    if (possibilities[4] == allSlots[r]) {
+                case reels[4]:
+                    if (reels[4] == allSlots[r]) {
                         bars++;
                     }
                     break;
-                case possibilities[5]:
-                    if (possibilities[5] == allSlots[r]) {
+                case reels[5]:
+                    if (reels[5] == allSlots[r]) {
                         knuckles++;
                     }
                     break;
-                case possibilities[6]:
-                    if (possibilities[6] == allSlots[r]) {
+                case reels[6]:
+                    if (reels[6] == allSlots[r]) {
                         rings++;
                     }
                     break;
-                case possibilities[7]:
-                    if (possibilities[7] == allSlots[r]) {
+                case reels[7]:
+                    if (reels[7] == allSlots[r]) {
                         blanks++;
                     }
                     break;
             }
         }
     }
-    total = [sonic, tails, yellowGuy, robotnic, bars, knuckles, rings, blanks];
 
-    for (var t = 0; t < total.length; t++) {
-        if (total[t] > highest) {
-            highest = total[t];
-            choice = t;
+    
+    if (blanks == 0) {
+        if (sonic == 3) {
+            winnings = playerBet * 10;
+            console.log("Win on Sonic: " + winnings);
         }
-    }
-    console.log("");
-    if (highest == 1) {
-        console.log("No matches");
-    }
-    if (highest == 2) {
-        console.log("Win on " + possibilities[choice]);
-        win++;
-    }
-    if (highest == 3) {
-        console.log("JackPot on " + possibilities[choice]);
-        win++;
-        jackpot++;
-    }
-    console.log("");
+        else if (tails == 3) {
+            winnings = playerBet * 20;
+            console.log("Win on Tails: " + winnings);
+        }
+        else if (yellowGuy == 3) {
+            winnings = playerBet * 30;
+            console.log("Win on yellowGuy: " + winnings);
+        }
+        else if (robotnic == 3) {
+            winnings = playerBet * 40;
+            console.log("Win on robotnic: " + winnings);
+        }
+        else if (bars == 3) {
+            winnings = playerBet * 50;
+            console.log("Win on bars: " + winnings);
+        }
+        else if (knuckles == 3) {
+            winnings = playerBet * 75;
+            console.log("Win on knuckles: " + winnings);
+        }
+        else if (rings == 3) {
+            winnings = playerBet * 100;
+            console.log("Win on rings: " + winnings);
+        }
+        else if (sonic == 2) {
+            winnings = playerBet * 2;
+            console.log("Win on Sonic: " + winnings);
+        }
+        else if (tails == 2) {
+            winnings = playerBet * 2;
+            console.log("Win on tails:" + winnings);
+        }
+        else if (yellowGuy == 2) {
+            winnings = playerBet * 3;
+            console.log("Win on yellowguy: " + winnings);
+        }
+        else if (robotnic == 2) {
+            winnings = playerBet * 4;
+            console.log("Win on robotnic: " + winnings);
+        }
+        else if (bars == 2) {
+            winnings = playerBet * 5;
+            console.log("Win on bars: " + winnings);
+        }
+        else if (knuckles == 2) {
+            winnings = playerBet * 10;
+            console.log("Win on knuckles: " + winnings);
+        }
+        else if (rings == 2) {
+            winnings = playerBet * 20;
+            console.log("Win on rings: " + winnings);
+        }
+        else {
+            winnings = playerBet * 1;
+            
+        }
 
-    console.log(choice);
-    console.log(highest);
-
-    switch (choice) {
-        case 0:
-            if (highest == 2) {
-                console.log("Won 0.1 on bet");
-            }
-            if (highest == 3) {
-                console.log("Won 1.1 on bet");
-            }
-            break;
-        case 1:
-            if (highest == 2) {
-                console.log("Won 0.2 on bet");
-            }
-            if (highest == 3) {
-                console.log("Won 1.2 on bet");
-            }
-            break;
-        case 2:
-            if (highest == 2) {
-                console.log("Won 0.3 on bet");
-            }
-            if (highest == 3) {
-                console.log("Won 1.3 on bet");
-            }
-            break;
-        case 3:
-            if (highest == 2) {
-                console.log("Won 0.4 on bet");
-            }
-            if (highest == 3) {
-                console.log("Won 1.4 on bet");
-            }
-            break;
-        case 4:
-            if (highest == 2) {
-                console.log("Won 0.5 on bet");
-            }
-            if (highest == 3) {
-                console.log("Won 1.5 on bet");
-            }
-            break;
-        case 5:
-            if (highest == 2) {
-                console.log("Won 0.6 on bet");
-            }
-            if (highest == 3) {
-                console.log("Won 1.6 on bet");
-            }
-            break;
-        case 6:
-            if (highest == 2) {
-                console.log("Won 0.7 on bet");
-            }
-            if (highest == 3) {
-                console.log("Won 1.7 on bet");
-            }
-            break;
-        case 7:
-            if (highest == 2) {
-                console.log("Won 0.8 on bet");
-            }
-            if (highest == 3) {
-                console.log("Won 1.8 on bet");
-            }
-            break;
+        win++;
+        // showWinMessage();
     }
+    else {
+        loss++;
+        console.log("Spin Again");
+        //  showLossMessage();
+    }
+
+    console.log("");
+    spins++;
+    console.log("Number is spins " + spins);
+    console.log("Number is wins " + win);
+    console.log("Number is losses " + loss);
+    console.log("Number of Jackpots " + jackpot);
+    console.log("jackPot Percentage " + Math.floor(jackpot / spins * 100) + " %");
+    console.log("Win percentage : " + Math.floor(win / spins * 100) + " %");
+    console.log("");
 }
 
 
